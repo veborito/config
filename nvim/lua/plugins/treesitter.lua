7411abe1f159757f06790eccb3109bff0fe5caff
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2024-01-23 08:48:49 +0100

    .config

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+  'nvim-treesitter/nvim-treesitter', 
+  build = ':TSUpdate',
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "cpp",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
